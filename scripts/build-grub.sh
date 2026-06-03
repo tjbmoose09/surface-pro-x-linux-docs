23a781b755c5d6efa1a6d98f60cff370d9df56ce
tjbmoose09 <tjboucaud@gmail.com>
2026-06-03 14:49:21 -0400

    Patch GRUB build for current Fedora toolchain

--- scripts/build-grub.sh
+++ scripts/build-grub.sh
@@ -44,11 +44,24 @@ modules_file="${grub_repo}/modules.txt"
 
 engine="$(detect_container_engine)" || die "podman or docker is required to build GRUB"
 image_name="spx-grub-aarch64"
+context_dir="${SPX_BUILD_DIR}/grub-build-context"
 ensure_dir "$out_dir" "$SPX_LOG_DIR/grub"
+
+rm -rf "$context_dir"
+ensure_dir "$context_dir"
+cp "${grub_repo}/Dockerfile" "${context_dir}/Dockerfile"
+cp "${grub_repo}/modules.txt" "${context_dir}/modules.txt"
+
+# The linux-surface GRUB branch currently fails with newer Fedora/GCC because
+# bootp.c still uses a 32-bit Unix timestamp where datetime.h now expects 64-bit.
+sed -i '/&& git checkout "${ref}"/a RUN cd grub \&\& sed -i '"'"'\'"'"'"'"'"'s/grub_int32_t t = 0;/grub_int64_t t = 0;/g'"'"'\'"'"'"'"'"' grub-core/net/bootp.c' \
+  "${context_dir}/Dockerfile"
+sed -i '/grub-core\/net\/bootp.c/a RUN cd grub \&\& sed -i '"'"'\'"'"'"'"'"'s/struct grub_net_bootp_packet \\*dhcp_ack = \\&pxe_mode->dhcp_ack;/struct grub_net_bootp_packet *dhcp_ack = (struct grub_net_bootp_packet *) \\&pxe_mode->dhcp_ack;/'"'"'\'"'"'"'"'"' grub-core/net/drivers/efi/efinet.c \&\& sed -i '"'"'\'"'"'"'"'"'s/struct grub_net_bootp_packet \\*proxy_offer = \\&pxe_mode->proxy_offer;/struct grub_net_bootp_packet *proxy_offer = (struct grub_net_bootp_packet *) \\&pxe_mode->proxy_offer;/'"'"'\'"'"'"'"'"' grub-core/net/drivers/efi/efinet.c' \
+  "${context_dir}/Dockerfile"
 
 info "Building GRUB container with ${engine}"
 run_logged "${SPX_LOG_DIR}/grub/container-build.log" \
-  "$engine" build -t "$image_name" "$grub_repo"
+  "$engine" build -t "$image_name" "$context_dir"
 
 mapfile -t modules < <(tr '[:space:]' '\n' < "$modules_file" | sed '/^$/d')
 
